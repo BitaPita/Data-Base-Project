7257083b56e64414115f9c8b4802321ebffb6ae4
Darya-83 <148362867+Darya-83@users.noreply.github.com>
2024-12-22 15:05:47 +0330

    Delete team directory

--- team/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from team.models import teams, drill
-
-
-admin.site.register(teams)
-admin.site.register(drill)
-
-# Register your models here.
--- team/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import  teamcreate, drillcreate, deletedrill, editdrill
-app_name= 'team'
-urlpatterns = [
-    path('addteam/', teamcreate, name='addteam'),
-    path('adddrill/', drillcreate, name='adddrill'),
-    path('editdrill/<int:pk>/',editdrill, name='editdrill'),
-    path('deletedrill/<int:pk>/', deletedrill, name='deletedrill')
-]
