fccf79d6472a6b5b968516af6916d79616052fc0
Darya-83 <148362867+Darya-83@users.noreply.github.com>
2024-12-22 15:06:06 +0330

    Delete users directory

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from users.models import User,playerProfile,coachProfile
-
-admin.site.register(User)
-admin.site.register(playerProfile)
-admin.site.register(coachProfile)
